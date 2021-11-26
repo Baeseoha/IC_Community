911b1eb1a6518a5f767d5b5803b62f26cf135e1b
baeseoha <65885458+Baeseoha@users.noreply.github.com>
2021-11-26 19:58:19 +0900

    서하 커뮤니티 첫번째 커

---
+++ src/main/java/com/lec/sprint/IcCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lec.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lec/sprint/IcCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
